f65209e2ba500263be2e7a67e96a261bd8b82804
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-05-10 13:15:55 +0300

    updates

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: true }" class="bg-green-100 shadow">
+<nav x-data="{ open: true }" class="sticky top-0 shadow-lg bg-green-100 shadow">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -15,21 +15,30 @@
                 <img class="hidden lg:block h-8 w-auto" src="/img/logos/workflow-logo-on-dark.svg" alt="" />
             </div>
             <div class="hidden sm:block sm:ml-6">
-                <div class="flex">
-                <a href="/" class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
-                <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
-                <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
-                <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+                <div class="leading-5 relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+                    <input
+                        type="text"
+                        class="bg-indigo-100 text-sm rounded w-64 px-4 pl-8 py-2 focus:outline-none focus:shadow-outline" placeholder="Search for..."
+                        x-ref="search"
+                    >
+                    <div class="z-50 absolute top-0">
+                        <svg class="text-gray-500 fill-current w-4 mt-2 ml-2" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 24 24" width="24" height="24">
+                            <path class="heroicon-ui"
+                                  d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
+                        </svg>
+                    </div>
                 </div>
+
             </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            @guest
-            <div class="flex">
-                <a href="{{ route('login') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Login</a>
-                <a href="{{ route('register') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Register</a>
-            </div>
-            @else
+{{--            @guest--}}
+{{--            <div class="flex">--}}
+{{--                <a href="{{ route('login') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Login</a>--}}
+{{--                <a href="{{ route('register') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Register</a>--}}
+{{--            </div>--}}
+{{--            @else--}}
             <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -38,6 +47,7 @@
             <div @click.away="open = false" class="ml-3 relative" x-data="{ open: false }">
                 <div>
                 <button @click="open = !open" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
+                    <div class="px-3 py-2 text-gray-500">John Doe</div>
                     <img class="h-8 w-8 rounded-full" src="/images/rix.jpg" alt="" />
                 </button>
                 </div>
@@ -56,16 +66,22 @@
                 </div>
                 </div>
             </div>
-            @endguest
+{{--            @endguest--}}
         </div>
       </div>
     </div>
     <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
       <div class="px-2 pt-2 pb-3">
-        <a href="/" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
-        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
-        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
-        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+          <div class="block relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+              <input
+                  type="text"
+                  class="bg-gray-200 text-sm rounded w-64 px-4 pl-8 py-2 focus:outline-none focus:shadow-outline" placeholder="Search for..."
+                  x-ref="search"
+              >
+              <div class="z-50 absolute top-0">
+                  <i class="fas fa-search text-gray-500 fill-current w-4 mt-2 ml-2"></i>
+              </div>
+          </div>
       </div>
     </div>
 </nav>
