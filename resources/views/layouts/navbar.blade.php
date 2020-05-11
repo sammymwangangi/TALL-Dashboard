64270ae938c504107164b62b1a08c97831cccba9
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-05-11 20:46:34 +0300

    clean design

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: true }" class="sticky top-0 shadow-lg bg-green-100 shadow">
+<nav x-data="{ open: true }" class="sticky top-0 shadow-lg bg-white shadow">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -18,7 +18,7 @@
                 <div class="leading-5 relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
                     <input
                         type="text"
-                        class="bg-indigo-100 text-sm rounded w-64 px-4 pl-8 py-2 focus:outline-none focus:shadow-outline" placeholder="Search for..."
+                        class="bg-gray-200 text-sm rounded w-64 px-4 pl-8 py-2 focus:outline-none focus:shadow-outline" placeholder="Search for..."
                         x-ref="search"
                     >
                     <div class="z-50 absolute top-0">
@@ -39,17 +39,17 @@
 {{--                <a href="{{ route('register') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-600 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Register</a>--}}
 {{--            </div>--}}
 {{--            @else--}}
-            <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
+            <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-100 hover:bg-gray-300 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                 </svg>
             </button>
             <div @click.away="open = false" class="ml-3 relative" x-data="{ open: false }">
                 <div>
-                <button @click="open = !open" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
-                    <div class="px-3 py-2 text-gray-500">John Doe</div>
-                    <img class="h-8 w-8 rounded-full" src="/images/rix.jpg" alt="" />
-                </button>
+                    <button @click="open = !open" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
+                        <div class="px-3 py-2 text-gray-500">John Doe</div>
+                        <img class="h-8 w-8 rounded-full" src="/images/rix.jpg" alt="" />
+                    </button>
                 </div>
                 <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
                 <div class="py-1 rounded-md bg-white shadow-xs">
