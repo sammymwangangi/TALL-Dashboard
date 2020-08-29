c75ec1d2aee1fc66ca2c33bbc8b9978124f89378
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-08-29 23:10:56 +0300

    updates

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -7,8 +7,8 @@
         'four' => 'border-orange-400 lg:ml-4'
     ]
 ])
-<div {{ $attributes->merge(['class' => "{$borders[$type]} transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-2xl shadow-xl rounded border-l-4 bg-white shadow-xl w-full h-auto text-center mb-2"]) }}>
-    <div class="flex items-center justify-between px-4 py-8">
+<div {{ $attributes->merge(['class' => "{$borders[$type]} transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-2xl shadow-xl rounded border-l-4 bg-white shadow-xl w-full h-24 text-center mb-2"]) }}>
+    <div class="flex items-center justify-between px-4 py-4">
         {{ $slot }}
     </div>
 </div>
