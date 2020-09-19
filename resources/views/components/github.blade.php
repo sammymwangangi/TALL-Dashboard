66656d75f8a40ed2098e88b0444511beb7807ebb
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-09-19 20:49:15 +0300

    Updates

--- resources/views/components/github.blade.php
+++ resources/views/components/github.blade.php
@@ -1,5 +1,5 @@
 <div class="h-auto bg-gradient-to-r from-yellow-500 via-pink-500 to-orange-400 mb-6">
-    <div class="px-8 py-8 grid grid-cols-4 gap-4">
+    <div class="px-8 py-8 md:grid md:grid-cols-4 gap-4">
         <div class="h-auto w-auto px-2 py-4 bg-white rounded transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100 hover:shadow-lg shadow border-b-8 border-orange-400 text-center">
             <button class="rounded-full bg-orange-200 p-2 focus:outline-none items-center justify-center">
                 <svg viewBox="0 0 20 20" fill="" class="fill-current text-orange-400 star w-8 h-8"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
