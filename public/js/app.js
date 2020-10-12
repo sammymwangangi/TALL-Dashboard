0638e8a837a0336d1e2fabb839bda3aa5677be65
Sammy Mwangangi <sammymwinzi2@gmail.com>
2020-10-12 17:28:18 +0300

    updated dependancies

--- public/js/app.js
+++ public/js/app.js
@@ -19395,8 +19395,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\foqus\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\foqus\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\xamppss\htdocs\foqus\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xamppss\htdocs\foqus\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
