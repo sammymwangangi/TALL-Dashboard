8c1c157ec54d21d45b576cae4af37f893e960b57
sammymwangangi <sammymwinzi2@gmail.com>
2021-02-08 20:00:09 +0300

    updated dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -19290,7 +19290,7 @@ process.umask = function() { return 0; };
 /******/ 	
 /******/ 	// the startup function
 /******/ 	// It's empty as some runtime module handles the default behavior
-/******/ 	__webpack_require__.x = x => {}
+/******/ 	__webpack_require__.x = x => {};
 /************************************************************************/
 /******/ 	/* webpack/runtime/global */
 /******/ 	(() => {
@@ -19306,7 +19306,7 @@ process.umask = function() { return 0; };
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
